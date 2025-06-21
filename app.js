683bec9e797ac0a30536d2f797453f38501fef85
Dennis <dennis-farris@hotmail.com>
2025-06-21 01:37:36 -0400

    basic html finished, javascript variables called

---
+++ app.js
@@ -0,0 +1,4 @@
+const taskNameInput = document.getElementById("new-task-input")
+const categoryInput = document.getElementById("category-input")
+const dateTimeInput = document.getElementById("date-time-input")
+const addTaskButton = document.getElementById("add-task")
