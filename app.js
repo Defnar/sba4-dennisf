57db16307b39b406f2128e453382bbe92424cb17
Dennis <dennis-farris@hotmail.com>
2025-06-21 04:01:04 -0400

    The filter list changes properly between the two.  Now working on the core functionality I was supposed to begin with

--- app.js
+++ app.js
@@ -6,62 +6,64 @@ const filterByDropdown = document.getElementById("type-dropdown");
 const filterSelectionDropdown = document.getElementById("filter");
 const taskGrid = document.getElementById("task-grid");
 
-
 let fullTaskList = [];
 let displayedTaskList = [];
 let categoryList = [];
+let filterByStatusList = ["Active", "Complete", "Overdue"];
 
-
-function Task(taskName, categoryName, timeDeadline, status="Active") {
-    this.name = taskName;
-    this.category = categoryName;
-    this.deadline = timeDeadline;
-    this.status = status;
+function Task(taskName, categoryName, timeDeadline, status = "Active") {
+  this.name = taskName;
+  this.category = categoryName;
+  this.deadline = timeDeadline;
+  this.status = status;
 }
 
-
-
+function optionBuilder(optionName) {
+  let option = document.createElement("option");
+  option.value = optionName;
+  option.textContent = optionName;
+  filterSelectionDropdown.appendChild(option);
+}
 
 function categoryBuilder() {
-    categoryList = [];
-    categoryList.push("any");
-    for (let task of fullTaskList) {
-        if (!categoryList.includes(task.category)) {
-            categoryList.push(task.category);
-        }
+  categoryList = [];
+  categoryList.push("Any");
+  for (let task of fullTaskList) {
+    if (!categoryList.includes(task.category)) {
+      categoryList.push(task.category);
     }
+  }
 }
 
-
-filterByDropdown.addEventListener("change", function() {
-    if (filterByDropdown.value == "category"){
-        filterSelectionDropdown.innerHTML = "";
-        categoryBuilder()
-        for (let cat  of categoryList) {
-            let option = document.createElement("option");
-            option.value = cat;
-            option.textContent = cat;
-            filterSelectionDropdown.appendChild(option);
-        }
+filterByDropdown.addEventListener("change", function () {
+  if (filterByDropdown.value == "category") {
+    filterSelectionDropdown.innerHTML = "";
+    categoryBuilder();
+    for (let cat of categoryList) {
+      optionBuilder(cat);
     }
-})
-
+  } else {
+    for (let filterStatus of filterByStatusList) {
+        optionBuilder(filterStatus)
+    }
+  }
+});
 
 function timeCheck(deadline) {
-    let deadlineTime = new Date(deadline);
-    let currentTime = new Date();
-    if (deadlineTime <= currentTime) {
-        return "Overdue";
-    }
-    else return "Active";
+  let deadlineTime = new Date(deadline);
+  let currentTime = new Date();
+  if (deadlineTime <= currentTime) {
+    return "Overdue";
+  } else return "Active";
 }
 
+addTaskButton.addEventListener("click", function () {
+  let newTask = new Task(
+    taskNameInput.value,
+    categoryInput.value,
+    dateTimeInput.value
+  );
+  fullTaskList.push(newTask);
+});
 
-addTaskButton.addEventListener("click", function() {
-    let newTask = new Task(taskNameInput.value, categoryInput.value, dateTimeInput.value);
-    fullTaskList.push(newTask);
-})
-
-function updateTaskView() {
-}
-
+function updateTaskView() {}
