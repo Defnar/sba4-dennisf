b97abc25495e95206cd0c69a47511817a832ff9c
Dennis <dennis-farris@hotmail.com>
2025-06-21 12:17:01 -0400

    Created basic card layout in javascript

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 //working on commenting more, there's a lot here to keep track of
-
-
 const taskNameInput = document.getElementById("new-task-input");
 const categoryInput = document.getElementById("category-input");
 const dateTimeInput = document.getElementById("date-time-input");
 const addTaskButton = document.getElementById("add-task");
-const filterByDropdown = document.getElementById("type-dropdown");
-const filterSelectionDropdown = document.getElementById("filter");
+const statusFilterDropdown = document.getElementById("status-filter");
+const categoryFilterDropdown = document.getElementById("category-filter");
 const taskGrid = document.getElementById("task-grid");
 
 let fullTaskList = [];
 let displayedTaskList = [];
 let categoryList = [];
-let filterByStatusList = ["Active", "Complete", "Overdue"];
 
 
 
@@ -22,46 +19,33 @@ function Task(taskName, categoryName, timeDeadline, status = "Active") {
   this.category = categoryName;
   this.deadline = timeDeadline;
   this.status = status;
-}
 
 
-//builds options for the filter selection box
-function optionBuilder(optionName) {
-  let option = document.createElement("option");
-  option.value = optionName;
-  option.textContent = optionName;
-  filterSelectionDropdown.appendChild(option);
+
+
 }
 
 
 
-//builds category list to use in making options for the dropdown menu
+
+//builds category list and uses it to create dropdown menu, only call when adding or deleting tasks
 function categoryBuilder() {
   categoryList = [];
   categoryList.push("Any");
+  categoryFilterDropdown.innerHTML = "";
   for (let task of fullTaskList) {
     if (!categoryList.includes(task.category)) {
       categoryList.push(task.category);
     }
   }
+  for (let category of categoryList) {
+    let option = document.createElement("option");
+  option.value = category;
+  option.textContent = category;
+  categoryFilterDropdown.appendChild(option);
+  }
 }
 
-
-
-//event listener to listen for first filter to change
-filterByDropdown.addEventListener("change", function () {
-  if (filterByDropdown.value == "category") {
-    filterSelectionDropdown.innerHTML = "";
-    categoryBuilder();
-    for (let cat of categoryList) {
-      optionBuilder(cat);
-    }
-  } else {
-    for (let filterStatus of filterByStatusList) {
-        optionBuilder(filterStatus)
-    }
-  }
-});
 
 //takes and compares deadline to current time
 function timeCheck(deadline) {
@@ -95,13 +79,43 @@ function testCase() {
 function cardBuilder(task) {
     let card = document.createElement('div');
     card.classList.add("card");
+
+    //checks status to see if card should be updated
+    if (task.status != "Complete") {
+      task.status = timeCheck(task.deadline);
+    }
+
+
+    const statusArr = ["Active", "Complete", "Overdue"];
+
+    let cardOptions = document.createElement("select")
+
+
+    //builds dropdown menu and selects corret option for status
+    for (let cardStatus of statusArr) {
+      let option = document.createElement("option")
+
+      if (task.status == cardStatus) {
+        option.selected = true;
+      }
+      option.value = cardStatus;
+      option.textContent = cardStatus;
+      cardOptions.appendChild(option)
+    }
+
+
+
+    //build card html
     card.innerHTML = `
     <p class="card-category">${task.category}</p>
     <h3 class="card-title">${task.name}</h3>
-    <p class="deadline">${new Date(task.deadline)}
-    
+    <p class="deadline">${new Date(task.deadline).toLocaleString()}</p>
     `
 
+    card.appendChild(cardOptions);
+
+    //returns card for update task view to handle
+    return card;
 }
 
 
@@ -110,5 +124,4 @@ function cardBuilder(task) {
 
 //updates the task view with the cards
 function updateTaskView() {
-    taskGrid.innerHTML = {};
 }
